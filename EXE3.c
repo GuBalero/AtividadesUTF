b1ba738ad0bf54870f88849a73f9ecb6d4b36553
Gustavo Balero Cosse de Sousa <gubalero@hotmail.com>
2019-08-19 13:40:11 -0300

    Create EXE3.c

---
+++ EXE3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+char converter(int nota, char * conceito);
+
+void main(){
+    int nota=9.1;
+    char conceito;
+    converter(nota, &conceito);
+    printf("%c", conceito);
+}
+
+char converter(int nota, char * conceito){
+    char c;
+
+    if(nota >= 9)
+        c = 'A';
+    else if(nota >=7)
+        c = 'B';
+    else if(nota >=6)
+        c = 'C';
+    else
+        c = 'I';
+
+    *conceito = c;
+}
