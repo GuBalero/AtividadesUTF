d935adb0cd21aaeb7cb1bca3d2a066b31ce695c2
Gustavo Balero Cosse de Sousa <gubalero@hotmail.com>
2019-08-20 16:16:46 -0300

    Update EXE1.c

--- EXE1.c
+++ EXE1.c
@@ -4,16 +4,16 @@ void potencia(int base, int expoente, int * res);
 
 void main(){
 
-    int base = 5, expoente = 2, resultado;
+    int base = 4, expoente = 2, resultado;
     potencia(base, expoente, &resultado);
     printf("%d", resultado);
 
 }
 
 void potencia(int base, int expoente, int * res){
-    int r=base;
+    int r=1;
     
-    for(int i = expoente-1; i>0; i--){
+    for(int i = expoente; i>0; i--){
         r = r * base;
     }
     *res = r;
