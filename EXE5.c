11ea71a9a97589b391b0314537114eb03ada5d23
Gustavo Balero Cosse de Sousa <gubalero@hotmail.com>
2019-08-20 00:48:39 -0300

    Update EXE5.c

--- EXE5.c
+++ EXE5.c
@@ -1,21 +1,34 @@
 #include <stdio.h>
 #define N 6
 
-void sacar(float valor, int * notas);
+void sacar(int valor, int * notas);
 
 void main(){
-    float valor=666;
+    int valor=666;
     int notas[N]; //NOTAS NO VETOR: 0 = 100; 1 = 50; 2 = 20; 3 = 10; 4 = 5; 5 = 2;
     sacar(valor, notas);
 }
 
-void sacar(float valor, int * notas){
+void sacar(int valor, int * notas){
     int saque[N] = {0}, valores[6] = {100,50,20,10,5,2};
 
     for(int i=0; i<N; i++){
         if(valor > 0){
+            if(i == N-2){
+                if(valor % 5 < 5){
+                    continue;
+                }
+            }
+
+            if(i == N-3){
+                if(valor % 10 < 5){
+                    continue;
+                }
+            }
+       
             saque[i] = valor/valores[i];
             valor -= saque[i] * valores[i];
+
         }else{
             break;
         }
