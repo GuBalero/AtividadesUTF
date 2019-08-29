48586c6763c53fa808a3352f764f9196fd93787d
Gustavo Balero Cosse de Sousa <gubalero@hotmail.com>
2019-08-29 20:38:04 -0300

    Create A05-3.c

---
+++ A05-3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void vetor_incrementa(int * vetor, int tam){
+    for(int i=0; i<tam; i++){
+        vetor[i] += 1;
+    }
+}
+
+void main(){
+    int v1[5]={10,20,30,40,50};
+
+    vetor_incrementa(v1, 5);
+
+    for(int i=0; i<5; i++){
+        printf("%d ", v1[i]);
+    }
+}
