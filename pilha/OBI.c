6b3c36fc4d28227b2937cac543798c54e15527dd
Gustavo Balero Cosse de Sousa <gubalero@hotmail.com>
2019-11-20 23:48:29 -0300

    update

---
+++ pilha/OBI.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int t;
+char a[100000];
+
+void main(){
+
+    scanf("%d ", &t);
+
+    for (int i = 0; i < t; i++)
+    {
+        scanf("[^\n] ", &a);
+
+        
+    }
+    
+
+
+}
+
